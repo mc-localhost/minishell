60043ce04704153740f1c4d5be926179701b8724
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-14 18:58:19 +0100

    improve error handle

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:31:13 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/02/14 18:46:53 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/02/14 18:56:03 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ int	cd(t_token *token, t_data *data)
 		printf("cd: string not in pwd: %s\n", token->args[0]);
 		return (0);
 	}
-	else if (chdir(token->args[0]) == 0)
+	else if (token->args_count == 1 && chdir(token->args[0]) == 0)
 	{
 		newpwd = ft_strjoin(find_env_var(&data->envs, "PWD"), "/");
 		if (newpwd)
@@ -34,7 +34,7 @@ int	cd(t_token *token, t_data *data)
 		}
 		free(newpwd);
 	}
-	else
+	else if (token->args_count == 1)
 	{
 	 	printf("cd: no such file or directory: %s\n", token->args[0]);
 		return (0);
