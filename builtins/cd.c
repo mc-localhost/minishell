2f3bbf6e2c15d5aa57273a744e07a08268a3ba50
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-22 00:00:15 +0100

    adjust CD to match bash

--- builtins/cd.c
+++ builtins/cd.c
@@ -6,19 +6,21 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/14 18:31:13 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/02/21 20:38:51 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/02/21 23:59:00 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
 
-static int	cd_error_x(char *str)
-{
-	ft_putstr_stderr("cd: string not in pwd: ");
-	ft_putstr_stderr(str);
-	ft_putstr_stderr("\n");
-	return (errno);
-}
+// static int	cd_error_x(char *str)
+// {
+// 	ft_putstr_stderr("cd: string not in pwd: ");
+// 	ft_putstr_stderr(str);
+// 	ft_putstr_stderr("\n");
+// 	return (errno);
+// }
+// if (token->args_count > 1)
+// 	return (cd_error_x(token->args[0]));
 
 int	error_notfound(char *str)
 {
@@ -32,9 +34,7 @@ int	cd(t_token *token, t_data *data)
 	char	cwd[1024];
 	char	*path;
 
-	if (token->args_count > 1)
-		return (cd_error_x(token->args[0]));
-	else if (token->args_count == 0 || !ft_strcmp(token->args[0], "~"))
+	if (token->args_count == 0 || !ft_strcmp(token->args[0], "~"))
 		path = find_env_var(&data->envs, "HOME");
 	else
 		path = token->args[0];
