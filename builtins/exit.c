67e19afd055acb06f8a8debbeb9da36646108996
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-21 21:52:13 +0100

    fix exit function

--- builtins/exit.c
+++ builtins/exit.c
@@ -3,22 +3,47 @@
 /*                                                        :::      ::::::::   */
 /*   exit.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: vvasiuko <vvasiuko@student.42.fr>          +#+  +:+       +#+        */
+/*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:01:56 by vvasiuko          #+#    #+#             */
-/*   Updated: 2025/02/17 17:48:23 by vvasiuko         ###   ########.fr       */
+/*   Updated: 2025/02/21 21:51:15 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../includes/minishell.h"
+
+int	ft_isnum(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] && str[i] != '\0')
+	{
+		if (!ft_isdigit(str[i]))
+			return (0);
+		i++;
+	}
+	return (1);
+}
 
 void	custom_exit(t_token *token) //not finished
 {
 	g_global.last_exit_code = 0;
 	if (token->args_count != 0)
 	{
-		if (token->args_count == 1)
-			g_global.last_exit_code = ft_atoi(token->args[0]);
+		if (token->args_count >= 1)
+		{
+			if (!ft_isnum(token->args[0]))
+			{
+				ft_putstr_stderr("exit: ");
+				ft_putstr_stderr(token->args[0]);
+				ft_putstr_stderr(" : numeric argument required");
+				g_global.last_exit_code = 255;
+			}
+			else
+				g_global.last_exit_code = ft_atoi(token->args[0]);
+
+		}
 	}
 	free_all();
 	unlink(HEREDOC_FILENAME);
