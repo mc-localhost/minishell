94b9b9ede39392a7adc24e82e410b82ee8c69933
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-21 23:36:27 +0100

    fix some cases for exit function

--- builtins/exit.c
+++ builtins/exit.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/17 16:01:56 by vvasiuko          #+#    #+#             */
-/*   Updated: 2025/02/21 22:31:34 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/02/21 23:36:15 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,6 +17,8 @@ int	ft_isnum(char *str)
 	int	i;
 
 	i = 0;
+	if (str[i] && (str[i] == '-' || str[i] == '+'))
+		i++;
 	while (str[i] && str[i] != '\0')
 	{
 		if (!ft_isdigit(str[i]))
@@ -26,34 +28,31 @@ int	ft_isnum(char *str)
 	return (1);
 }
 
-void	custom_exit(t_token *token) //not finished
+void	custom_exit(t_token *token)
 {
-	int noprint;
-
-	g_global.last_exit_code = errno;
-	noprint = 0;
+	g_global.last_exit_code = 0;
 	if (token->args_count != 0)
 	{
+		ft_putstr_stderr("exit\n");
 		if (token->args_count >= 1)
 		{
 			if (!ft_isnum(token->args[0]))
 			{
-				ft_putstr_stderr("exit\nbash: exit: ");
+				ft_putstr_stderr("bash: exit: ");
 				ft_putstr_stderr(token->args[0]);
 				ft_putstr_stderr(" : numeric argument required");
 				g_global.last_exit_code = 255;
-				noprint = 1;
 			}
 			else if (token->args_count == 1)
 				g_global.last_exit_code = ft_atoi(token->args[0]);
 			else if (token->args_count > 1)
-				return (ft_putstr_stderr("exit\nbash: exit: too many arguments\n"));
+				return (ft_putstr_stderr("bash: exit: too many arguments\n"));
 		}
 	}
+	else
+		printf("exit\n");
 	free_all();
 	unlink(HEREDOC_FILENAME);
-	if (!noprint)
-		printf("exit\n");
 	exit(g_global.last_exit_code);
 }
 
