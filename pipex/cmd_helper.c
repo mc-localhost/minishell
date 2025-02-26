c966f1dc86acd1888bead85469f8c6904e8ab579
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-26 16:58:19 +0100

    fix error codes

--- pipex/cmd_helper.c
+++ pipex/cmd_helper.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/11/30 20:11:27 by aelaaser          #+#    #+#             */
-/*   Updated: 2025/02/23 18:26:58 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/02/26 16:55:33 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -92,11 +92,23 @@ char	*find_path(char *cmd, char **envp)
 {
 	int		i;
 
+	if (!ft_strcmp(cmd, ".") || !ft_strcmp(cmd, ".."))
+		return (0);
 	if (access(cmd, F_OK) == 0)
 		return (cmd);
+	i = 0;
+	if (cmd[0] == '~')
+	{
+		cmd = ft_strtrim(cmd, "~");
+		cmd = ft_strtrim(cmd, "/");
+		while (envp[i] && ft_strnstr(envp[i], "HOME", 4) == 0)
+			i++;
+		if (!envp[i])
+			return (0);
+		return (get_path(cmd, envp[i]));
+	}
 	if (ft_strchr(cmd, '/'))
 		return (0);
-	i = 0;
 	while (envp[i] && ft_strnstr(envp[i], "PATH", 4) == 0)
 		i++;
 	if (!envp[i])
