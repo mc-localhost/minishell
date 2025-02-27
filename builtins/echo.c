ce0eb5ceb6de535417096d583317aad6c2774f6a
Ahmed Elaaser <ahmed.abuelmakarem@gmail.com>
2025-02-27 18:35:10 +0100

    function for esc chart

--- builtins/echo.c
+++ builtins/echo.c
@@ -6,7 +6,7 @@
 /*   By: aelaaser <aelaaser@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/15 15:59:55 by vvasiuko          #+#    #+#             */
-/*   Updated: 2025/02/27 03:52:05 by aelaaser         ###   ########.fr       */
+/*   Updated: 2025/02/27 18:33:57 by aelaaser         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -50,6 +50,33 @@ static int	echo_n(t_token *token, int start)
 	if (token->args[i])
 		printf("%s", token->args[i]);
 	return (0);
+}
+// in case if we want to use esc chart
+char	*esc_chart(char	*str)
+{
+	int		i;
+	int		j;
+	char	*s;
+
+	i = 0;
+	j = 0;
+	s = malloc(ft_strlen(str) * sizeof(char));
+	if (!s)
+		return (str);
+	while (str[i])
+	{
+		if (str[i + 1] && str[i] == '\\' && str[i + 1] == '\\')
+		{
+			s[j++] = '\\';
+			i += 2;
+		}
+		if (str[i] == '\\')
+			i++;
+		s[j++] = str[i];
+		i++;
+	}
+	s[j] = '\0';
+	return (s);
 }
 
 int	echo(t_token *token)
